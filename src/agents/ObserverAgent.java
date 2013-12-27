1cd2bc20098a05432344633530dbfe31173a6068
Umutcan Simsek <s.umutcan@gmail.com>
2013-12-27 23:43:59 +0200

    observeragent created

---
+++ src/agents/ObserverAgent.java
@@ -0,0 +1,13 @@
+package agents;
+
+import jade.core.Agent;
+
+public class ObserverAgent extends Agent {
+
+	public void setup()
+	{
+		System.out.println("Selamun Aleyküm mümin kardeş. Ben gözlemciyim elhamdülillah.");
+	}
+	
+
+}
