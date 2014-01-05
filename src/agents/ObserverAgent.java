d50895249c1b529e7360e4a7f3ca9f2e15ec52d9
Umutcan Simsek <s.umutcan@gmail.com>
2014-01-05 18:56:34 +0200

    observeragent observing

--- src/agents/ObserverAgent.java
+++ src/agents/ObserverAgent.java
@@ -1,6 +1,11 @@
 package agents;
 
+
+import behaviours.Ping;
+import behaviours.Pong;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.FSMBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -9,6 +14,8 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class ObserverAgent extends Agent {
 
+	AID[] allAgents = null;
+	
 	public void setup()
 	{
 		System.out.println("Observer agent up and running.");
@@ -27,12 +34,20 @@ public class ObserverAgent extends Agent {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+				allAgents = new AID[results.length];
 				for (int i = 0; i<results.length; i++)
 				{
+					allAgents[i] = results[i].getName();
 					System.out.println("Agent: " + results[i].getName().getLocalName());
 				}
 				
+				FSMBehaviour observing = new FSMBehaviour();
+				observing.registerFirstState(new Ping(allAgents), "PING");
+				observing.registerLastState(new Pong(), "PONG");
+				observing.registerDefaultTransition("PING", "PONG");
+				observing.registerDefaultTransition("PONG", "PING");
+				
+				addBehaviour(observing);
 			}
 		});
 	}
---
+++ src/behaviours/Ping.java
@@ -0,0 +1,43 @@
+package behaviours;
+
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class Ping extends Behaviour {
+
+	AID[] allAgents = null;
+	MessageTemplate mt = null;
+	
+	public Ping(AID[] allAgents)
+	{
+		this.allAgents = allAgents;
+	}
+	@Override
+	public void action() {
+		
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+		
+		for(AID agent : allAgents)
+			msg.addReceiver(agent);
+		
+		msg.setContent("IS_ALIVE");
+		msg.setConversationId("observing");
+		msg.setReplyWith("request"+System.currentTimeMillis());
+		
+		mt = MessageTemplate.and(MessageTemplate.MatchConversationId("observing"),
+				MessageTemplate.MatchInReplyTo(msg.getReplyWith()));
+		
+		myAgent.send(msg);
+		
+
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
